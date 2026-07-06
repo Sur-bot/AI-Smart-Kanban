401c74430d0b7861e91a471a05e8370d208d46c0
AnhNguyen <www.vanhsp2002@gmail.com>
2026-07-06 14:27:01 +0700

    feat(header/invite): Implement UI invite element

---
+++ src/app/layout/header/actions-button/invite/invite.ts
@@ -0,0 +1,34 @@
+import { Component, HostListener, ElementRef, Output, EventEmitter, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate } from '@angular/animations';
+
+@Component({
+  selector: 'app-invite-dropdown',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './invite.html',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(40px)' }),
+        animate('250ms cubic-bezier(0.2, 0.8, 0.2, 1)', style({ opacity: 1, transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({ opacity: 0, transform: 'translateY(40px)' }))
+      ])
+    ])
+  ]
+})
+export class InviteComponent {
+  @Input() isOpen = false;
+  @Output() close = new EventEmitter<void>();
+
+  constructor(private eRef: ElementRef) {}
+
+  @HostListener('document:click', ['$event'])
+  clickout(event: any) {
+    if (this.isOpen && !this.eRef.nativeElement.contains(event.target)) {
+      this.close.emit();
+    }
+  }
+}
--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SubNavComponent } from './sub-nav/sub-nav';
 import { SearchComponent } from './search/expandable-search';
 import { SettingSliderComponent } from './setting/setting-slider';
+import { InviteComponent } from './actions-button/invite/invite'; // Import popup mới
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.html',
   styleUrls: ['./header.scss'],
   standalone: true,
-  imports: [CommonModule, SubNavComponent, SearchComponent, SettingSliderComponent],
+  imports: [CommonModule, SubNavComponent, SearchComponent, SettingSliderComponent, InviteComponent],
 })
 export class HeaderComponent {
   currentView = signal<'list' | 'board'>('board');
@@ -30,5 +31,12 @@ export class HeaderComponent {
   togglePopup(event: Event) {
     event.stopPropagation(); // Ngăn click lan ra ngoài làm đóng popup ngay lập tức
     this.isPopupOpen = !this.isPopupOpen;
+  }
+
+  isInviteOpen = false;
+
+  toggleInvite(event: Event) {
+    event.stopPropagation();
+    this.isInviteOpen = !this.isInviteOpen;
   }
 }
