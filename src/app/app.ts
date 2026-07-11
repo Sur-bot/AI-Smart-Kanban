b27393999bce4a81ccdd912f6f8c506176691d8e
AnhNguyen <www.vanhsp2002@gmail.com>
2026-07-11 23:23:01 +0700

    build(i18n): setup ngx-translate and migrate assets to public dir for Angular 18

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-    imports: [RouterOutlet, CommonModule ],
-    templateUrl: './app.html',
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.html',
   styleUrl: './app.scss',
   standalone: true
 })
 export class App {
   protected readonly title = signal('ai-smart-kanban');
+
+  constructor(private translate: TranslateService) {
+    this.translate.use('vi');
+  }
 }
